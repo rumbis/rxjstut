248fa362b99e2b238cf176c3b2147cf33932c420
George Alexandrou <cysystemcy@gmail.com>
2022-05-05 01:11:46 +0300

    i comment out all the console.logto receive only the message of observer

--- index.js
+++ index.js
@@ -59,21 +59,21 @@ const observable = new Observable((subscriber) => {
    
 }).pipe(
     map((value) => {
-        console.log("1) got data from observable ", value)
+        // console.log("1) got data from observable ", value)
        return value.data
     }),
     map((value) => {
 
-        console.log("2)Got data from first operator ", value)
+        // console.log("2)Got data from first operator ", value)
        return value.filter(user => user.status === "active")
         }),
         map((value) => {
 
-            console.log("3)Got data from second operator ", value)
+            // console.log("3)Got data from second operator ", value)
            return (value.reduce((sum, user) => sum+ user.age, 0) / value.length )
             }),
             map ((value) => {
-                console.log("4) Got data from third operator", value);
+                // console.log("4) Got data from third operator", value);
                 if(value < 18) throw new Error("Age is less than 18");
                 else return value;
             }),
